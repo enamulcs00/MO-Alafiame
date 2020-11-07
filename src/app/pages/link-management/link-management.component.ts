900b05e30db2c89bc7203515d9541942f55f46cb
Shivangi Dubey <node-trainee@mobiloitte.com>
2020-11-07 16:25:09 +0530

    changes done

--- src/app/pages/link-management/link-management.component.ts
+++ src/app/pages/link-management/link-management.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MainService } from 'src/app/provider/main.service';
+import { FormGroup, FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-link-management',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./link-management.component.css']
 })
 export class LinkManagementComponent implements OnInit {
+  link: any;
 
-  constructor() { }
+  constructor(private activate:ActivatedRoute,private route:Router,public mainService: MainService) { }
 
   ngOnInit() {
+    this.getLink();
+  }
+  getLink() {
+    this.mainService.showSpinner();
+    this.mainService.getApi('appSharing/appSharingData',1).subscribe((res: any) => {
+      if (res.responseCode == 200) {
+        this.link = res.result;
+        // this.form.patchValue({
+        //   "question": this.faqList.question,
+        //   "answer": this.faqList.message
+        // })
+        this.mainService.hideSpinner();
+        this.mainService.successToast(res.responseMessage);
+      } else {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(res.responseMessage)
+      }
+    })
   }
 
 }
