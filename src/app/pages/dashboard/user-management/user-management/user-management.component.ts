77a7af4d71c190c2a7fbccda40888efcbca06169
Enamul haque <enamulcs008@gmail.com>
2020-12-01 15:21:40 +0530

    service management Api and its delete Api are added

--- src/app/pages/dashboard/user-management/user-management/user-management.component.ts
+++ src/app/pages/dashboard/user-management/user-management/user-management.component.ts
@@ -440,7 +440,7 @@ export class UserManagementComponent implements OnInit {
       'limit':this.itemPerPage,
     }
     this.mainService.postApi('admin/corporateList',data, 1).subscribe((res:any)=>{
-      
+      console.log('Response of corporate--:',res)
       if(res.responseCode==200){
         this.mainService.hideSpinner();
         this.corporateData=res.result.docs;
@@ -587,10 +587,6 @@ export class UserManagementComponent implements OnInit {
       }
     })
   }
-
-  
-
-
   changeCorporateValue(){
     this.getCorporate()
     this.corporateUserValue=true;
--- src/app/pages/home-visit-service/home-visit-service.component.ts
+++ src/app/pages/home-visit-service/home-visit-service.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MainService } from 'src/app/provider/main.service';
+declare var $: any;
 
 @Component({
   selector: 'app-home-visit-service',
@@ -6,16 +9,135 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-visit-service.component.css']
 })
 export class HomeVisitServiceComponent implements OnInit {
-  search:any;
-  total:any;
-  constructor() { }
+  search: string;
+  currentPage: number = 1;
+  itemPerPage:number=10;
+servicelists: any=[];
+categoryList: any=[];
+  categoryId: string;
+ 
 
-  ngOnInit() {}
+  constructor(private router: Router,public mainService: MainService) { }
 
-  searchValue(){}
+  ngOnInit() {
+    this.categoryLists();
+    this.serviceList();
+  }
+  searchValue() {
+   
+    this.mainService.showSpinner();
+    let object = {
+      "search": this.search,
+      "page": this.currentPage,
+      "limit": this.itemPerPage
+      }
+    this.mainService.postApi('admin/categoryList', object, 1).subscribe(res => {
+      console.log(" productList==>", res)
+      if (res.responseCode == 200 && res.result) {
+        this.mainService.hideSpinner();
+        this.mainService.successToast(res.responseMessage)
+        this.categoryList = res.result.docs
+      } else {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(res.responseMessage)
+      }
+      error => {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(error.responseMessage)
+      }
+    })
+    
+    
+  }
+  categoryLists()
+  {
+    this.mainService.showSpinner();
+    
+    let object = {
+      "page": this.currentPage,
+      "limit": this.itemPerPage
+      }
+    this.mainService.postApi('admin/categoryList',object,1).subscribe(res => {
+      console.log(" productList==>", res)
+      if (res.responseCode == 200 && res.result && res.result.docs) {
+        console.log('shweta',res.result)
+        this.categoryList = res.result.docs
+        this.mainService.hideSpinner();
+        this.mainService.successToast(res.responseMessage)
+        
+        
+      } else {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(res.responseMessage)
+      }
+      error => {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(error.responseMessage)
+      }
+    })
+  }
+  serviceList()
+  {
+     this.mainService.showSpinner();
+    
+    let object = {
+      "page": this.currentPage,
+      "limit": this.itemPerPage
+      }
+    this.mainService.postApi('admin/serviceList',object,1).subscribe(res => {
+      console.log(" productList==>", res)
+      if (res.responseCode == 200 && res.result && res.result.docs) {
+        console.log('shweta',res.result)
+        this.servicelists = res.result.docs
+        this.mainService.hideSpinner();
+        this.mainService.successToast(res.responseMessage)
+        
+        
+      } else {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(res.responseMessage)
+      }
+      error => {
+        this.mainService.hideSpinner();
+        this.mainService.errorToast(error.responseMessage)
+      }
+    })
 
-  pagination(event){}
+  }
 
-  deleteUser(){}
+  
+  deleteFunction(id) {
+     this.categoryId  = id
+     $('#deleteModal').modal({ backdrop: 'static', keyboard: false })
+  }
+  deleteUser()
+  {
+    this.mainService.showSpinner()
+    let object = {
+      'categoryId': this.categoryId
+    }
+
+    this.mainService.deleteApi('admin/deleteCategory',object,1).subscribe(res => {
+     if (res.responseCode == 200) {
+        this.mainService.hideSpinner()
+        $('#deleteModal').modal('hide');
+        this.mainService.successToast(res.responseMessage)
+       
+      } else {
+        this.mainService.hideSpinner()
+        this.mainService.errorToast(res.responseMessage)
+      }
+    }, error => {
+      this.mainService.hideSpinner()
+      this.mainService.errorToast(error.responseMessage)
+    })
+  }
+
+ pagination(event) {
+  console.log(event)
+  this.currentPage = event;
+  this.serviceList();
+}
+
 
 }
