da875064f4dbed546e6bdf3decf37a752b359fbe
ph-sauravsharma <ph-sauravsharma@mobiloitte.com>
2020-12-11 18:17:57 +0530

    Add subcategory api integration

--- src/app/pages/home-visit-service/view-home-visit/view-home-visit.component.ts
+++ src/app/pages/home-visit-service/view-home-visit/view-home-visit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MainService } from 'src/app/provider/main.service';
 declare var $: any;
@@ -13,11 +14,14 @@ export class ViewHomeVisitComponent implements OnInit {
   profile: any;
   id: any;
   subcategorList: any=[];
+  addSubForm: FormGroup;
   constructor(private activate:ActivatedRoute,private route:Router,public mainService: MainService) { }
   ngOnInit() {
     this.activate.paramMap.subscribe(params => {
       this.categoryId  = params.get('id');
-      
+    });
+    this.addSubForm = new FormGroup({
+      "categoryName": new FormControl('', Validators.required),
     });
     this.viewCategory();
   }
@@ -68,4 +72,52 @@ export class ViewHomeVisitComponent implements OnInit {
      this.mainService.errorToast(error.responseMessage)
    })
  }
+
+ openAdd(){
+ 
+  $('#addSub').modal('show')
+
+ }
+
+
+ addSubService(){
+  let data = 
+  {
+    'categoryId':this.categoryId,
+    'subCategoryName': this.addSubForm.value.categoryName,
+    'subCategoryImage': this.profile,
+  }
+  this.mainService.showSpinner();
+  this.mainService.postApi('admin/addService', data, 1).subscribe((res: any) => {
+    console.log("addProduct response ==>", res)
+    if (res.responseCode == 200 && res.result) {
+      this.mainService.hideSpinner();
+      this.mainService.successToast(res.responseMessage)
+      $('#addSub').modal('hide');
+
+    } else {
+      this.mainService.hideSpinner();
+      this.mainService.errorToast(res.responseMessage)
+    }
+    error => {
+      this.mainService.hideSpinner();
+      this.mainService.errorToast(error.responseMessage)
+    }
+  })
+
+ }
+
+
+ handleInputChange(e) {
+  var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+  
+  var reader = new FileReader();
+  reader.onload = this._handleReaderLoaded.bind(this);
+  reader.readAsDataURL(file);
+}
+_handleReaderLoaded(e) {
+  let reader = e.target;
+  this.profile = reader.result;
+  console.log("profile", this.profile)
+}
 }
